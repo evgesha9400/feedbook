254d5a2d0000592f0cdb9722b89049b93b8bf5b2
Evgeny Aleshin <aleshiner@mail.ru>
2019-04-23 00:24:09 +0100

    update

--- feedbook/settings.py
+++ feedbook/settings.py
@@ -155,7 +155,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
