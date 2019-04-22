a3763d0526e05431a35129963f66406fe1fa6285
Evgeny Aleshin <aleshiner@mail.ru>
2019-04-23 00:38:01 +0100

    update

--- feedbook/settings.py
+++ feedbook/settings.py
@@ -152,6 +152,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGGING = {
